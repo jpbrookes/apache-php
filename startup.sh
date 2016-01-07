e9bfecf4d1714ff807c30a7a61ad711b99499c31
jpbrookes <jpbrookes@googlemail.com>
2016-01-07 15:05:19 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "git clone https://github.com/jpbrookes/php_app.git ."
 git clone https://github.com/jpbrookes/php_app.git .
+echo "git remote update ."
 git remote update .
+echo "git pull origin master"
 git pull origin master
